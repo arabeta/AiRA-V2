2fa7b587db4a4096dd9b601b49b4bbee6a33cb08
randomroot1598 <67897779+randomroot1598@users.noreply.github.com>
2022-06-05 23:47:49 -0400

    init

---
+++ aira-v2/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="align-middle">
+      <div className="text-center text-3xl  ">
+        Hello ARA!
+      </div>
+    </div>
+  );
+}
+
+export default App;
